35a7ab342a009c941def34dd9c660aeb0656fd40
julienbrg <julien@strat.cc>
2023-09-07 12:33:53 +0200

    add getRate

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -4,18 +4,26 @@ import fs from 'fs'
 
 async function main() {
 
-  const initialMint = ethers.parseEther("10000")
-  const Basic = await ethers.getContractFactory("Basic");
-  const basic = await Basic.deploy(initialMint);
+  const DEX = await ethers.getContractFactory("DEX");
+  const dex1 = await DEX.deploy(1);
 
-  const recordAddress = {
-    "contractAddress": await basic.getAddress()
+  const DEX2 = await ethers.getContractFactory("DEX");
+  const dex2 = await DEX2.deploy(2);
+
+  const RateAggregator = await ethers.getContractFactory("RateAggregator");
+  const rateAggregator = await RateAggregator.deploy(await dex1.getAddress(), await dex2.getAddress());
+
+  const recordAddresses = {
+    "rateAggregator": await rateAggregator.getAddress(),
+    "dex1": await dex1.getAddress(),
+    "dex2": await dex1.getAddress(),
+
   }
   
-  const content = JSON.stringify(recordAddress, null, 2);
-  fs.writeFileSync('store.json', content);
+  const content = JSON.stringify(recordAddresses, null, 2);
+  fs.writeFileSync('addresses.json', content);
 
-  console.log('Basic ERC-20 token contract deployed:', msg(await basic.getAddress()));
+  console.log('Rate Aggregator contract deployed:', msg(await rateAggregator.getAddress()));
 }
 
 main().catch((error) => {
